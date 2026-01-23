5bc9da9b2df26e64b9898bc46f13a403fa6c872c
aravinth1228 <aravinthkalai1228@gmail.com>
2026-01-23 12:36:41 +0530

    React

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import Courses from './course';
+
+function App() {
+  return (
+    <>  
+      <Courses/>
+    </>
+  );
+}
+
+export default App
---
+++ vite-project/src/course.jsx
@@ -0,0 +1,17 @@
+ 
+function Courses(){
+    return(
+
+        <>
+         
+         <h1>Hi Im Aravinth</h1>
+         <p>we are learning React js</p>
+         <img src= "" alt="" />
+
+        
+        </>
+    )
+
+}
+
+export default Courses
