3a3f83a39820c1ff3b1b9c8dba30012053bef747
Aravinth1228 <aravinthkalai1228gmail.com>
2026-01-25 11:55:29 +0530

    react

--- vite-project/src/course.jsx
+++ vite-project/src/course.jsx
@@ -1,15 +1,34 @@
- 
-function Courses(){
-    return(
+import { useState } from "react";
 
-        <>
-         
-         <h1>Hi Im Aravinth</h1>
-         <p>we are learning React js</p>
-         <img src= "" alt="" />
-        </>
-    )
+function Courses() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("Aravinth");
+  const [gender, setGender] = useState("Male");
 
+  function handleClick() {
+    setCount((prev) => {
+      const newCount = prev + 1;
+
+      if (newCount % 2 === 0) {
+        setName("ShreePriyanka"); // even
+        setGender("Female");
+      } else {
+        setName("Aravinth"); // odd
+        setGender("Male");
+      }
+
+      return newCount;
+    });
+  }
+
+  return (
+    <>
+      <h1>Hi I'm {name}</h1>
+      <p>We are learning React JS</p>
+      <button onClick={handleClick}>Click Me</button>
+        <p>Gender: {gender}</p>
+    </>
+  );
 }
 
-export default Courses
+export default Courses;
