225dfd6d6eb3b1ea510bc857587d94a46c8981f6
aravinth1228 <aravinthkalai1228@gmail.com>
2026-01-28 09:18:10 +0530

    add

--- vite-project/src/course.jsx
+++ vite-project/src/course.jsx
@@ -1,19 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Courses() {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("Aravinth");
   const [gender, setGender] = useState("Male");
+  const [users, setUsers] = useState([]);
 
   function handleClick() {
     setCount((prev) => {
       const newCount = prev + 1;
 
       if (newCount % 2 === 0) {
-        setName("ShreePriyanka"); // even
+        setName("ShreePriyanka");
         setGender("Female");
       } else {
-        setName("Aravinth"); // odd
+        setName("Aravinth");
         setGender("Male");
       }
 
@@ -21,12 +22,23 @@ function Courses() {
     });
   }
 
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
   return (
     <>
       <h1>Hi I'm {name}</h1>
-      <p>We are learning React JS</p>
+      <p>Gender: {gender}</p>
+
       <button onClick={handleClick}>Click Me</button>
-        <p>Gender: {gender}</p>
+
+      <h2>Users List</h2>
+      {users.map((user) => (
+        <p key={user.id}>{user.name},{user.email}</p>
+      ))}
     </>
   );
 }
