80dbb45f179ba1a12d51c981a6d7e3b9033bd7b2
aravinth7 <aravinth@shamlatech.net>
2026-02-24 17:35:08 +0530

    new

--- Instgram/src/App.jsx
+++ Instgram/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import Sidebar from './sidebar'
 function App() {
   return (
-    <div className='d-flex'>
-       <div>sidebor</div>
-       <div>feed</div>
-       <div>suddestion</div>
+    <div className='d-flex vh-100 transparent'>
+       <div className='w-20 '><Sidebar/></div>
+       <div className='w-50  bg-info text - p-3'>feed</div>
+       <div className='w-30 '>suddestion</div>
        
     </div>
   )
---
+++ Instgram/src/sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function sidebar() {
+  return (
+    <>
+    <div>
+      <img className ='text-logo'src=".\src\assets\download.png" alt="" />
+      <h1>
+        Aravinth
+      </h1>
+    </div>
+     </>
+  )
+}
+
+export default sidebar
