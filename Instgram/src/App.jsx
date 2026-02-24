9d7dad52360a7b0d5474898399cea49196d628c9
aravinth7 <aravinth@shamlatech.net>
2026-02-24 16:16:50 +0530

    React

---
+++ Instgram/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='d-flex'>
+       <div>sidebor</div>
+       <div>feed</div>
+       <div>suddestion</div>
+       
+    </div>
+  )
+}
+
+export default App
