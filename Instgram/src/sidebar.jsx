6ea6c9036915f5cecd8863d4435d235e151b433c
aravinth7 <aravinth@shamlatech.net>
2026-02-26 12:21:23 +0530

    Add

--- Instgram/src/sidebar.jsx
+++ Instgram/src/sidebar.jsx
@@ -2,14 +2,24 @@ import React from 'react'
 
 function sidebar() {
   return (
-    <>
-    <div>
-      <img className ='text-logo'src=".\src\assets\download.png" alt="" />
-      <h1>
-        Aravinth
-      </h1>
+    <div className='m-3'>
+        <div className='d-flex flex-column gap-3'>
+          <img className ='text-logo'src=".\src\assets\download.png" alt="" />
+          <div><i class="bi bi-house-door-fill"></i>Home</div>
+          <div><i class="bi bi-search"></i>Search</div>
+          <div><i class="bi bi-compass"></i>Explore</div>
+          <div><i class="bi bi-play-btn"></i>Reels</div>
+          <div><i class="bi bi-chat-heart-fill"></i>Messages</div>
+          <div><i class="bi bi-bell-fill"></i>Notification</div>
+          <div><i class="bi bi-plus-square"></i>Create</div>
+          <div><i class="bi bi-person-circle"></i>Profile</div>
+        </div>
+
+        <div className=' position-fixed bottom-0 d-flex flex-column gap-3 mb-5'>
+          <div><i class="bi bi-threads"></i>Threads</div>
+          <div><i class="bi bi-list"></i>More</div>
+        </div>
     </div>
-     </>
   )
 }
 
